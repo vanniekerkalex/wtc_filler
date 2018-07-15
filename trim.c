82204f3b753fb1a57b9a95f6b123a78a80bcf36b
Alex van Niekerk <alexvanniekerk@Alexs-MacBook-Pro.local>
2018-07-15 21:02:40 +0200

    removed spaces in trim.c

--- trim.c
+++ trim.c
@@ -14,106 +14,106 @@
 
 void   ft_trim_top(struct maps maps)
 {
-    int i;
-    int j;
-    int count;
-    struct token t;
+	int i;
+	int j;
+	int count;
+	struct token t;
 
-    t.row_top = 0;
-    i = 0;
-    count = 0;
-    while (i < maps.dim_ty)
-    {
-        j = 0;
-        while (j < maps.dim_tx)
-        {
-            if (maps.t_map[i][j++] != '.')
-                count++;
-        }
-        if (count > 0)
-            break ;
-        else
-            t.row_top++;
-        i++;
-    }
-    ft_trim_bottom(maps, t);
+	t.row_top = 0;
+	i = 0;
+	count = 0;
+	while (i < maps.dim_ty)
+	{
+		j = 0;
+		while (j < maps.dim_tx)
+		{
+			if (maps.t_map[i][j++] != '.')
+				count++;
+		}
+		if (count > 0)
+			break ;
+		else
+			t.row_top++;
+			i++;
+	}
+	ft_trim_bottom(maps, t);
 }
 
 void   ft_trim_bottom(struct maps maps, struct token t)
 {
-    int i;
-    int j;
-    int count;
+	int i;
+	int j;
+	int count;
 
-    t.row_bottom = 0;
-    i = maps.dim_ty - 1;
-    count = 0;
-    while (i >= 0)
-    {
-        j = maps.dim_tx - 1;
-        while (j >= 0)
-        {
-            if (maps.t_map[i][j--] != '.')
-                count++;
-        }
-        if (count > 0)
-            break ;
-        else
-            t.row_bottom++;
-        i--;
-    }
-    ft_trim_left(maps, t);
+	t.row_bottom = 0;
+	i = maps.dim_ty - 1;
+	count = 0;
+	while (i >= 0)
+	{
+		j = maps.dim_tx - 1;
+		while (j >= 0)
+		{
+			if (maps.t_map[i][j--] != '.')
+				count++;
+		}
+		if (count > 0)
+			break ;
+		else
+			t.row_bottom++;
+		i--;
+	}
+	ft_trim_left(maps, t);
 }
 
 void   ft_trim_left(struct maps maps, struct token t)
 {
-    int i;
-    int j;
-    int count;
-    struct token t;
+	int i;
+	int j;
+	int count;
+	struct token t;
 
-    t.col_left = 0;
-    j = 0;
-    count = 0;
-    while (j < maps.dim_tx)
-    {
-        i = 0;
-        while (i < maps.dim_ty)
-        {
-            if (maps.t_map[i++][j] != '.')
-                count++;
-        }
-        if (count > 0)
-            break ;
-        else
-            t.col_left++;
-        j++;
-    }
-    ft_trim_right(maps, t);
+	t.col_left = 0;
+	j = 0;
+	count = 0;
+	while (j < maps.dim_tx)
+	{
+		i = 0;
+		while (i < maps.dim_ty)
+		{
+			if (maps.t_map[i++][j] != '.')
+				count++;
+		}
+		if (count > 0)
+			break ;
+		else
+			t.col_left++;
+		j++;
+	}
+	ft_trim_right(maps, t);
 }
 
 struct token    ft_trim_bottom(struct maps maps, struct token t)
 {
-    int i;
-    int j;
-    int count;
+	int i;
+	int j;
+	int count;
 
-    t.row_bottom = 0;
-    j = maps.dim_tx - 1;
-    count = 0;
-    while (j >= 0)
-    {
-        i = maps.dim_ty - 1;
-        while (i >= 0)
-        {
-            if (maps.t_map[i--][j] != '.')
-                count++;
-        }
-        if (count > 0)
-            return (t);
-        else
-            t.row_bottom++;
-        j--;
-    }
-    return (t);
+	t.row_bottom = 0;
+	j = maps.dim_tx - 1;
+	count = 0;
+	while (j >= 0)
+	{
+		i = maps.dim_ty - 1;
+		while (i >= 0)
+		{
+			if (maps.t_map[i--][j] != '.')
+				count++;
+		}
+		if (count > 0)
+			return (t);
+		else
+			t.row_bottom++;
+		j--;
+	}
+	return (t);
 }
